a15b10aa780e462ca957a28ba9c242ae8bd62f8a
beatriz9938-maker <beatriz9938@gmail.com>
2025-11-12 10:40:47 -0300

    primeiro commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+
+function Header (){
+    return <header>
+        <img src="" alt="Logomarca da página" />
+        <ul>
+        <li><a href="#home">LOOKS</a></li>
+        <li><a href="#about">Lançamentos</a></li>
+        <li><a href="#services">Novidades</a></li>
+        <li><a href="#contact">Contact</a></li>
+        </ul>
+    </header>
+}
+export default Header
